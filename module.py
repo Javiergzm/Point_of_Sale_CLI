3f7293e22749889fea8ab059a215081d872f8bb4
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2026-04-14 17:53:01 +0000

    hi

--- module.py
+++ module.py
@@ -10,13 +10,18 @@ class PointOfSale:
     self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
     self.cart = []
     
-    print("\nInitializing POS system...")
+    print("\nInitializing Javi's POS system...")
     
     
   def start(self): # This is the function that should be used to start the application.
-    print("Menu! ")
     print("1: Show shopping cart ")
     print("2: Add Items to the cart ")
     print("3: Display Total")
     print("4: Pay")
     print("5: Bye, Bye! ")
+    while True:
+      print("Menu please select an option! ")
+      choice = input
+      if choice == "1":
+        print("self.cart")
+      else if choice == "2"
